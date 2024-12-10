289550b033757776062feee18709974594a311b6
Daniel Cano <60793959+dcanogi@users.noreply.github.com>
2024-12-10 21:54:42 +0100

    Update test_app.py

--- Dockerized-web-deploy-kubernetes-with-github-actions/app/test_app.py
+++ Dockerized-web-deploy-kubernetes-with-github-actions/app/test_app.py
@@ -1,16 +1,17 @@
-# test_app.py
-import pytest
+import unittest
 from app import app
 
-# El cliente de prueba de Flask
-@pytest.fixture
-def client():
-    with app.test_client() as client:
-        yield client
+class AppTestCase(unittest.TestCase):
+    # El cliente de prueba de Flask
+    def setUp(self):
+        self.app = app.test_client()
 
-# Test para la ruta /api/data
-def test_get_data(client):
-    response = client.get('/api/data')  # Realiza una solicitud GET
-    json_data = response.get_json()  # Obtén los datos JSON de la respuesta
-    assert response.status_code == 200  # Verifica que el código de estado sea 200.
-    assert json_data['message'] == 'Hello from Python!'  # Verifica el mensaje
+    # Test para la ruta /api/data
+    def test_get_data(self):
+        response = self.app.get('/api/data')  # Realiza una solicitud GET
+        json_data = response.get_json()  # Obtén los datos JSON de la respuesta
+        self.assertEqual(response.status_code, 200)  # Verifica que el código de estado sea 200.
+        self.assertEqual(json_data['message'], 'Hello from Python!')  # Verifica el mensaje
+
+if __name__ == '__main__':
+    unittest.main()
