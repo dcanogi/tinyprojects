6b38f5eb839b39809df4106e054c181611d8da6d
Daniel Cano <60793959+dcanogi@users.noreply.github.com>
2024-12-10 22:02:10 +0100

    Update test_app.py

--- Dockerized-web-deploy-kubernetes-with-github-actions/app/test_app.py
+++ Dockerized-web-deploy-kubernetes-with-github-actions/app/test_app.py
@@ -8,7 +8,7 @@ class AppTestCase(unittest.TestCase):
 
     # Test para la ruta /api/data
     def test_get_data(self):
-        response = self.app.get('/api/data')  # Realiza una solicitud GET
+        response = self.app.get('/api/data')  # Realiza una solicitud GET.
         json_data = response.get_json()  # Obtén los datos JSON de la respuesta
         self.assertEqual(response.status_code, 200)  # Verifica que el código de estado sea 200.
         self.assertEqual(json_data['message'], 'Hello from Python!')  # Verifica el mensaje
